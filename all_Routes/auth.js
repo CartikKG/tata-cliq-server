9b0a26d2b33acc3b95dcafa80c4824115bac3f6c
Kartik Gupta <109219148+CartikKG@users.noreply.github.com>
2023-01-02 22:51:23 +0530

    Added

--- all_Routes/auth.js
+++ all_Routes/auth.js
@@ -1,15 +1,45 @@
 const router = require("express").Router();
+const User=require('../models/users.model')
 const passport = require("passport");
+const config = require("../config/config");
+const jwt = require("jsonwebtoken");
+function generateToken(user) {
+  if (user.password) {
+    delete user.password;
+  }
+  return jwt.sign(user, config.JWT_SECRET_KEY);
+}
 
 const CLIENT_URL = "http://localhost:3000/";
 
-router.get("/login/success", (req, res) => {
+router.get("/login/success",async (req, res) => {
   if (req.user) {
-    res.status(200).json({
-      success: true,
-      message: "successfull",
-      user: req.user,
-    });
+   
+    let name=req.user.displayName;
+    let email=req.user.emails[0].value;
+    let avatar=req.user.photos[0].value;
+    console.log(name,email,avatar);
+    let exitisUser = await User.findOne({ email });
+    if (exitisUser) {
+       exitisUser=exitisUser.toJSON();
+       let token= generateToken(exitisUser);
+       res.send({
+        token:token
+       });
+    }else{
+       let user = await User.create({
+         name,
+         email,
+         avatar,
+         authType: "google",
+       });
+       user = user.toJSON();
+       let token= generateToken(user);
+       res.send({
+        token:token
+       });
+        
+    }
   }
 });
 
@@ -25,7 +55,7 @@ router.get("/logout", (req, res) => {
   res.redirect(CLIENT_URL);
 });
 
-router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+router.get("/google", passport.authenticate("google", { scope: ['email', 'profile'] }));
 
 router.get(
   "/google/callback",
@@ -45,14 +75,14 @@ router.get(
   })
 );
 
-router.get("/facebook", passport.authenticate("facebook",  { scope: ['user_friends', 'manage_pages'] } ));
+router.get("/facebook", passport.authenticate("facebook", { scope: ["profile"] }));
 
 router.get(
-	"/facebook/callback",
-	passport.authenticate("facebook", {
-		successRedirect: process.env.CLIENT_URL,
-		failureRedirect: "/login/failed",
-	})
+  "/facebook/callback",
+  passport.authenticate("facebook", {
+    successRedirect: CLIENT_URL,
+    failureRedirect: "/login/failed",
+  })
 );
 
 module.exports = router
