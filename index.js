ca9fed854599169589498b308577751283b342c9
Kartik Gupta <109219148+CartikKG@users.noreply.github.com>
2023-01-03 00:33:26 +0530

    ..

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(passport.session());
  
 app.use(
     cors({
-        origin:process.env.CLIENT_URL,
+        origin:'http://localhost:3000',
         methods:"GET,POST,PUT,DELETE",
         credentials:true
  
